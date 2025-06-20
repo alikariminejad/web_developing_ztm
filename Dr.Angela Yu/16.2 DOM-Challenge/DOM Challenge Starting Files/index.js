3d6f6a782c530609ea58c88e36d646116d3b67b7
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-06-20 17:39:12 +0330

    working with js, dom

---
+++ Dr.Angela Yu/16.2 DOM-Challenge/DOM Challenge Starting Files/index.js
@@ -0,0 +1,12 @@
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Angela";
+document.querySelector("button").style.backgroundColor = "yellow";
+
+
+// document.querySelector("button").classList.add("invisible");
+// document.querySelector("button").classList.remove("invisible");
+// document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").innerHTML = "<em> Good Bye </em>";
+// document.querySelector("h1").innerText = "Hello again";
+document.querySelector("a").getAttribute("href"); // = "https://www.google.com" 
+document.querySelector("a").setAttribute("href", "https://www.abc.com");
